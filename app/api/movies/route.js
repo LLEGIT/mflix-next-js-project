65d4cbaea9beaf656371287a72ce68400ff37764
theo.gillet <theo.gillet@ekino.com>
2025-03-27 10:05:51 +0100

    fix: added movies limit retrieval

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -19,7 +19,7 @@ export async function GET() {
     try {
         const client = await clientPromise;
         const db = client.db('sample_mflix');
-        const movies = await db.collection('movies').find({}).toArray();
+        const movies = await db.collection('movies').find({}).limit(50).toArray();
 
         return NextResponse.json(
             { status: 200, data: movies }
